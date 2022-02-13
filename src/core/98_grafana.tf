8aaa1b9bc7475b25402f119a3e3c32782dc09173
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-02-13 17:18:45 +0100

    Add Grafana infrastructure

---
+++ src/core/98_grafana.tf
@@ -0,0 +1,34 @@
+resource "azurerm_resource_group" "grafana_rg" {
+  name     = "${local.project}-grafana-rg"
+  location = var.location
+
+  tags = var.tags
+}
+
+module "grafana_app_docker" {
+  source = "git::https://github.com/pagopa/azurerm.git//app_service?ref=v2.2.0"
+
+  name                = "${local.project}-grafana"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.grafana_rg.name
+
+  plan_type     = "internal"
+  plan_name     = "${local.project}-grafana-plan"
+  plan_kind     = "Linux"
+  plan_sku_tier = "Basic"
+  plan_sku_size = "B1"
+  plan_reserved = true
+
+  always_on         = true
+  linux_fx_version  = "DOCKER|grafana/grafana-oss:latest"
+  health_check_path = "/api/health"
+
+  app_settings = {
+    #APPINSIGHTS_INSTRUMENTATIONKEY                  = azurerm_application_insights.application_insights.instrumentation_key
+    #APPLICATIONINSIGHTS_CONNECTION_STRING           = format("InstrumentationKey=%s", azurerm_application_insights.application_insights.instrumentation_key)
+    # WEBSITES_ENABLE_APP_SERVICE_STORAGE           = false
+    WEBSITES_PORT                                   = 3000
+  }
+
+  tags = var.tags
+}
