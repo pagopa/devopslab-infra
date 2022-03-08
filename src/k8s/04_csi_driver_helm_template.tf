ed7428189f6048a9b2f89227c38afd943d798279
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-03-08 14:06:31 +0100

    Add permission to get secrets to CSI identity

---
+++ src/k8s/04_csi_driver_helm_template.tf
@@ -0,0 +1,7 @@
+resource "azurerm_key_vault_access_policy" "cluster_access_policy" {
+  key_vault_id = data.azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_subscription.current.tenant_id
+  object_id    = "25859118-4d19-418a-97ff-97af3a4ca929"
+
+  secret_permissions      = ["get"]
+}
