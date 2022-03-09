9939d3bdd4a78a932a3b55a5eb1d4a3aaddd226c
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-03-09 15:30:01 +0100

    Set CSI secret identity dryinamically

--- src/k8s/04_csi_key_vault_access_policy.tf
+++ src/k8s/04_csi_key_vault_access_policy.tf
@@ -1,7 +1,13 @@
+locals {
+  secrets_provider = data.azurerm_kubernetes_cluster.aks_cluster.addon_profile[0].azure_keyvault_secrets_provider[0]
+}
+
 resource "azurerm_key_vault_access_policy" "cluster_access_policy" {
+  count = local.secrets_provider.enabled ? 1 : 0
+
   key_vault_id = data.azurerm_key_vault.kv.id
   tenant_id    = data.azurerm_subscription.current.tenant_id
-  object_id    = "25859118-4d19-418a-97ff-97af3a4ca929"
+  object_id    = local.secrets_provider.secret_identity[0].object_id
 
   secret_permissions = ["get"]
 }
