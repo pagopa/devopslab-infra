cfc26861f9a41025cfd73e83541122bb17b3df73
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-05-21 17:06:09 +0200

    pre-commit fix

--- src/matteo/01_aks.tf
+++ src/matteo/01_aks.tf
@@ -80,13 +80,9 @@ module "aks" {
   addon_azure_key_vault_secrets_provider_enabled = var.aks_addons.azure_key_vault_secrets_provider
   addon_azure_pod_identity_enabled               = var.aks_addons.pod_identity_enabled
 
-<<<<<<<< HEAD:src/matteo/01_aks.tf
   default_metric_alerts = var.aks_metric_alerts_default
   custom_metric_alerts  = var.aks_metric_alerts_custom
 
-========
-  metric_alerts  = var.aks_metric_alerts
->>>>>>>> origin/main:src/core/03_aks.tf
   alerts_enabled = var.aks_alerts_enabled
 
   action = [
