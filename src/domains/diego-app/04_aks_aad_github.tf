2bf57c931972383b302a1f173638ef8cd2fde058
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-14 15:11:37 +0100

    removed permissions not needed

--- src/domains/diego-app/04_aks_aad_github.tf
+++ src/domains/diego-app/04_aks_aad_github.tf
@@ -25,12 +25,12 @@ resource "azurerm_role_assignment" "aks_service_cluster_user_role_for_github_run
   ]
 }
 
-# this was made because we need a way to achive this role: Microsoft.ContainerService/managedClusters/accessProfiles/listCredential/action
-resource "azurerm_role_assignment" "aks_service_rbac_admin_for_github_runner_ci" {
-  scope                = data.azurerm_kubernetes_cluster.aks.id
-  role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
-  principal_id         = data.azuread_service_principal.github_runner_ci.id
-}
+# # this was made because we need a way to achive this role: Microsoft.ContainerService/managedClusters/accessProfiles/listCredential/action
+# resource "azurerm_role_assignment" "aks_service_rbac_admin_for_github_runner_ci" {
+#   scope                = data.azurerm_kubernetes_cluster.aks.id
+#   role_definition_name = "Azure Kubernetes Service RBAC Cluster Admin"
+#   principal_id         = data.azuread_service_principal.github_runner_ci.id
+# }
 
 
 resource "azurerm_key_vault_access_policy" "github_runner_ci" {
@@ -72,29 +72,29 @@ resource "null_resource" "aks_with_iac_aad_plus_namespace_ci" {
   }
 }
 
-resource "null_resource" "aks_with_iac_aad_plus_namespace_system_ci" {
+# resource "null_resource" "aks_with_iac_aad_plus_namespace_system_ci" {
 
-  triggers = {
-    aks_id               = data.azurerm_kubernetes_cluster.aks.id
-    service_principal_id = data.azuread_service_principal.github_runner_ci.id
-    namespace            = var.domain
-  }
+#   triggers = {
+#     aks_id               = data.azurerm_kubernetes_cluster.aks.id
+#     service_principal_id = data.azuread_service_principal.github_runner_ci.id
+#     namespace            = var.domain
+#   }
 
-  provisioner "local-exec" {
-    command = <<EOT
-      az role assignment create --role "Azure Kubernetes Service RBAC Admin" \
-      --assignee ${self.triggers.service_principal_id} \
-      --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}-system
-    EOT
-  }
+#   provisioner "local-exec" {
+#     command = <<EOT
+#       az role assignment create --role "Azure Kubernetes Service RBAC Admin" \
+#       --assignee ${self.triggers.service_principal_id} \
+#       --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}-system
+#     EOT
+#   }
 
-  provisioner "local-exec" {
-    when    = destroy
-    command = <<EOT
-      az role assignment delete --role "Azure Kubernetes Service RBAC Admin" \
-      --assignee ${self.triggers.service_principal_id} \
-      --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}-system
-    EOT
-  }
-}
+#   provisioner "local-exec" {
+#     when    = destroy
+#     command = <<EOT
+#       az role assignment delete --role "Azure Kubernetes Service RBAC Admin" \
+#       --assignee ${self.triggers.service_principal_id} \
+#       --scope ${self.triggers.aks_id}/namespaces/${self.triggers.namespace}-system
+#     EOT
+#   }
+# }
 
