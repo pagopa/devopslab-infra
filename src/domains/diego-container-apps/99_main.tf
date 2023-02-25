079c503bcbf418dfb835e15239fa28fcd938fdb9
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-25 23:38:21 +0100

    azurerm upgrated to 3.45

--- src/domains/diego-container-apps/99_main.tf
+++ src/domains/diego-container-apps/99_main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "<= 3.45.0"
+      version = "= 3.45.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
@@ -36,16 +36,16 @@ data "azurerm_subscription" "current" {}
 
 data "azurerm_client_config" "current" {}
 
-data "terraform_remote_state" "core" {
-  backend = "azurerm"
+# data "terraform_remote_state" "core" {
+#   backend = "azurerm"
 
-  config = {
-    resource_group_name  = var.terraform_remote_state_core.resource_group_name
-    storage_account_name = var.terraform_remote_state_core.storage_account_name
-    container_name       = var.terraform_remote_state_core.container_name
-    key                  = var.terraform_remote_state_core.key
-  }
-}
+#   config = {
+#     resource_group_name  = var.terraform_remote_state_core.resource_group_name
+#     storage_account_name = var.terraform_remote_state_core.storage_account_name
+#     container_name       = var.terraform_remote_state_core.container_name
+#     key                  = var.terraform_remote_state_core.key
+#   }
+# }
 
 provider "kubernetes" {
   config_path = "${var.k8s_kube_config_path_prefix}/config-${local.aks_name}"
