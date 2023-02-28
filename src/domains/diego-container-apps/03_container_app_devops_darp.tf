2a6d48168add54a476aa445b861586b4dba1450a
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-02-28 10:20:09 +0100

    added backendapp

--- src/domains/diego-container-apps/03_container_app_devops_darp.tf
+++ src/domains/diego-container-apps/03_container_app_devops_darp.tf
@@ -17,6 +17,9 @@ data "azurerm_container_app_environment" "dapr_env" {
   ]
 }
 
+#
+# Frontend
+#
 resource "azurerm_container_app" "frontend" {
   name                         = "frontend-dapr-showcase"
   container_app_environment_id = data.azurerm_container_app_environment.dapr_env.id
@@ -24,12 +27,12 @@ resource "azurerm_container_app" "frontend" {
   revision_mode                = "Single"
 
   template {
-    min_replicas = 0
+    min_replicas = 1
     max_replicas = 1
 
     container {
       name   = "frontend"
-      image  = "ghcr.io/pagopa/devops-webapp-python:v1.0.0"
+      image  = "ghcr.io/pagopa/devops-webapp-python:beta-colors"
       cpu    = 0.5
       memory = "1Gi"
 
@@ -59,6 +62,68 @@ resource "azurerm_container_app" "frontend" {
       latest_revision = true
       percentage      = 100
     }
+  }
+
+  dapr {
+    app_id = "frontend"
+    app_port = 8000
+  }
+
+  depends_on = [
+    data.azurerm_container_app_environment.dapr_env
+  ]
+}
+
+#
+# backend
+#
+resource "azurerm_container_app" "backend" {
+  name                         = "backend-dapr-showcase"
+  container_app_environment_id = data.azurerm_container_app_environment.dapr_env.id
+  resource_group_name          = azurerm_resource_group.container_app_diego.name
+  revision_mode                = "Single"
+
+  template {
+    min_replicas = 1
+    max_replicas = 1
+
+    container {
+      name   = "backend"
+      image  = "ghcr.io/diegoitaliait/dapr-showcase:beta-enable-dapr"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      liveness_probe {
+        failure_count_threshold = 10
+        initial_delay           = 10
+        interval_seconds        = 10
+        path                    = "/status"
+        port                    = 3000
+        transport               = "HTTP"
+      }
+
+      readiness_probe {
+        failure_count_threshold = 10
+        interval_seconds        = 10
+        path                    = "/status"
+        port                    = 3000
+        transport               = "HTTP"
+      }
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 3000
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  dapr {
+    app_id = "backend"
+    app_port = 3000
   }
 
   depends_on = [
