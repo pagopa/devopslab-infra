f2628c05b0cc1b72bd4d2b1555d3c6ad7115cfda
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-03-05 18:23:06 +0100

    fix outputs in app-github-runner-ci

--- .identity/modules/app-github-runner-ci/output.tf
+++ .identity/modules/app-github-runner-ci/output.tf
@@ -1,8 +1,16 @@
-output "azure_environment_ci" {
-  value = {
-    app_name       = var.app_name
-    client_id      = azuread_service_principal.environment_ci.application_id
-    application_id = azuread_service_principal.environment_ci.application_id
-    object_id      = azuread_service_principal.environment_ci.object_id
-  }
+output "app_name" {
+  value = var.app_name
+}
+
+output "client_id" {
+  value = azuread_service_principal.environment_ci.application_id
+
+}
+
+output "application_id" {
+  value = azuread_service_principal.environment_ci.application_id
+}
+
+output "object_id" {
+  value = azuread_service_principal.environment_ci.object_id
 }
