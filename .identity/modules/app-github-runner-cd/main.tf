484b7cdefc045051d25531dfb572e1ef28326e62
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-03-06 14:12:22 +0100

    created module app-github-runner-cd

---
+++ .identity/modules/app-github-runner-cd/main.tf
@@ -0,0 +1,50 @@
+data "azurerm_storage_account" "tfstate_storage" {
+  name                = var.tfstate_storage_account_name
+  resource_group_name = var.tfstate_storage_account_rg_name
+}
+
+data "azuread_group" "group_github_runners_iac_permissions" {
+  display_name = var.iac_aad_group_name
+}
+
+#
+# Create App
+#
+resource "azuread_application" "github_app" {
+  display_name = var.app_name
+}
+
+resource "azuread_service_principal" "github_app" {
+  application_id = azuread_application.github_app.application_id
+}
+
+resource "azuread_application_federated_identity_credential" "github_app" {
+  application_object_id = azuread_application.github_app.object_id
+  display_name          = "github-federated"
+  description           = "github-federated"
+  audiences             = ["api://AzureADTokenExchange"]
+  issuer                = "https://token.actions.githubusercontent.com"
+  subject               = "repo:${var.github_org}/${var.github_repository}:environment:${var.github_environment_name}"
+}
+
+# assign SP to group with Directory Reader
+resource "azuread_group_member" "add_github_runner_to_directory_readers_group" {
+  group_object_id  = data.azuread_group.group_github_runners_iac_permissions.id
+  member_object_id = azuread_service_principal.github_app.object_id
+}
+
+#
+# Assign roles
+#
+resource "azurerm_role_assignment" "github_app_subscription" {
+  for_each             = toset(local.github_app_roles.subscription)
+  scope                = var.subscription_id
+  role_definition_name = each.key
+  principal_id         = azuread_service_principal.github_app.object_id
+}
+
+resource "azurerm_role_assignment" "environment_cd_tfstate_inf" {
+  scope                = data.azurerm_storage_account.tfstate_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azuread_service_principal.github_app.object_id
+}
