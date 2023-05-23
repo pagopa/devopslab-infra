c7fef2160d6dfaeb53cf8c95a35bf73537070bd4
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-05-23 16:50:25 +0200

    refactor variables

--- src/pillar/04_azure_devops_agent.tf
+++ src/pillar/04_azure_devops_agent.tf
@@ -16,6 +16,9 @@ module "azdoa_snet" {
   private_endpoint_network_policies_enabled = true
 }
 
+
+#module packer
+
 module "azdoa_vmss_li" {
   source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=v4.1.0"
   count               = var.enable_azdoa ? 1 : 0
@@ -28,26 +31,31 @@ module "azdoa_vmss_li" {
 }
 
 
-module "azdoa_vmss_li_2" {
-  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=e48e8d8"
-  count               = var.enable_azdoa ? 1 : 0
-  name                = "${local.azuredevops_agent_vm_name}-new"
-  resource_group_name = azurerm_resource_group.azdo_rg[0].name
-  subnet_id           = module.azdoa_snet[0].id
-  subscription        = data.azurerm_subscription.current.display_name
-  enable_disk_encryption = false
-
-  tags = var.tags
-}
-
-# commented, needs fixes to handle admin ssh key
 #module "azdoa_vmss_li_2" {
-#  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=cc45d62"
+#  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=20f02c4"
 #  count               = var.enable_azdoa ? 1 : 0
-#  name                = "${local.azuredevops_agent_vm_name}-tf"
+#  name                = "${local.azuredevops_agent_vm_name}-new"
 #  resource_group_name = azurerm_resource_group.azdo_rg[0].name
 #  subnet_id           = module.azdoa_snet[0].id
 #  subscription        = data.azurerm_subscription.current.display_name
+#  enable_disk_encryption = false
 #
 #  tags = var.tags
 #}
+
+# commented, needs fixes to handle admin ssh key
+module "azdoa_vmss_li_2" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent_custom_image?ref=1eaef02"
+  count               = var.enable_azdoa ? 1 : 0
+  resource_group_name = azurerm_resource_group.azdo_rg[0].name
+  location            = var.location
+  image_name = "my_image_name"
+  image_version = "v1"
+  subscription_id = data.azurerm_subscription.current.subscription_id
+
+
+
+  tags = var.tags
+
+}
+
