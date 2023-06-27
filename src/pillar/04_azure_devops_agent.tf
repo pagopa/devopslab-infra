4ab7c4b80088c35359329f730221713780063104
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-06-27 11:38:38 +0200

    upgrated module version

--- src/pillar/04_azure_devops_agent.tf
+++ src/pillar/04_azure_devops_agent.tf
@@ -18,7 +18,7 @@ module "azdoa_snet" {
 
 
 module "azdoa_vmss_li" {
-  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=v6.13.0"
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=v6.20.0"
   count               = var.enable_azdoa ? 1 : 0
   name                = local.azuredevops_agent_vm_name
   resource_group_name = azurerm_resource_group.azdo_rg[0].name
@@ -27,7 +27,6 @@ module "azdoa_vmss_li" {
   subscription_id     = data.azurerm_subscription.current.subscription_id
   location            = var.location
   source_image_name   = var.azdoa_image_name
-  image_type          = "custom"
 
   tags = var.tags
 }
