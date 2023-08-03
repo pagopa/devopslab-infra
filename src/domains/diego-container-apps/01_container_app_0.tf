546e200acfc1f584cba7e182368aedd7dbb2c787
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-08-03 18:26:38 +0200

    added features flags

--- src/domains/diego-container-apps/01_container_app_0.tf
+++ src/domains/diego-container-apps/01_container_app_0.tf
@@ -8,7 +8,6 @@ resource "null_resource" "update_az_cli" {
   triggers = {
     env_name                                   = local.container_app_diego_environment_name
     rg                                         = azurerm_resource_group.container_app_diego.name
-    subnet_id                                  = module.container_apps_snet.id
     log_analytics_id                           = data.azurerm_log_analytics_workspace.log_analytics.workspace_id
     log_analytics_workspace_primary_shared_key = data.azurerm_log_analytics_workspace.log_analytics.primary_shared_key
   }
--- src/domains/diego-container-apps/01_cosmos_db.tf
+++ src/domains/diego-container-apps/01_cosmos_db.tf
@@ -1,13 +1,16 @@
 resource "azurerm_resource_group" "cosmosdb_dapr" {
+  count    = var.is_resource_enabled.mongodb_dapr ? 1 : 0
   name     = "${local.project}-dapr-cosmos-rg"
   location = var.location
   tags     = var.tags
 }
 
-resource "azurerm_cosmosdb_account" "mongodb" {
+resource "azurerm_cosmosdb_account" "mongodb_dapr" {
+  count = var.is_resource_enabled.mongodb_dapr ? 1 : 0
+
   name                = "${local.project}-dapr-cosmos"
-  location            = azurerm_resource_group.cosmosdb_dapr.location
-  resource_group_name = azurerm_resource_group.cosmosdb_dapr.name
+  location            = azurerm_resource_group.cosmosdb_dapr[0].location
+  resource_group_name = azurerm_resource_group.cosmosdb_dapr[0].name
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
 
@@ -28,16 +31,20 @@ resource "azurerm_cosmosdb_account" "mongodb" {
 }
 
 resource "azurerm_cosmosdb_sql_database" "db_sql_dapr" {
+  count = var.is_resource_enabled.mongodb_dapr ? 1 : 0
+
   name                = local.cosmosdb_db_name
-  resource_group_name = azurerm_resource_group.cosmosdb_dapr.name
-  account_name        = azurerm_cosmosdb_account.mongodb.name
+  resource_group_name = azurerm_resource_group.cosmosdb_dapr[0].name
+  account_name        = azurerm_cosmosdb_account.mongodb_dapr[0].name
 }
 
 resource "azurerm_cosmosdb_sql_container" "collection_dapr" {
+  count = var.is_resource_enabled.mongodb_dapr ? 1 : 0
+
   name                = local.cosmosdb_collection_name
-  resource_group_name = azurerm_resource_group.cosmosdb_dapr.name
-  account_name        = azurerm_cosmosdb_account.mongodb.name
-  database_name       = azurerm_cosmosdb_sql_database.db_sql_dapr.name
+  resource_group_name = azurerm_resource_group.cosmosdb_dapr[0].name
+  account_name        = azurerm_cosmosdb_account.mongodb_dapr[0].name
+  database_name       = azurerm_cosmosdb_sql_database.db_sql_dapr[0].name
 
   partition_key_path = "/id"
 }
