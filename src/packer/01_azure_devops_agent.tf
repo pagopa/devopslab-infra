82406536f6f97dce3b367c997e42c30a4ba49e4c
mamari90 <130982006+mamari90@users.noreply.github.com>
2023-09-27 17:10:28 +0200

    updated agent module

--- src/packer/01_azure_devops_agent.tf
+++ src/packer/01_azure_devops_agent.tf
@@ -4,12 +4,12 @@ data "azurerm_resource_group" "resource_group" {
 
 
 module "azdoa_custom_image" {
-  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent_custom_image?ref=3a39074"
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent_custom_image?ref=v7.8.0"
   resource_group_name = data.azurerm_resource_group.resource_group.name
   location            = var.location
   image_name          = "azdo-agent-ubuntu2204-image-velero"
-  image_version       = "v1"
+  image_version       = "v2"
   subscription_id     = data.azurerm_subscription.current.subscription_id
-
+  prefix              = "devopla"
   tags = var.tags
 }
