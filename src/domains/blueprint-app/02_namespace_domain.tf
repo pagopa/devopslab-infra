ffb705170f1c2f7ed55dfbbd78936c8f14c058a3
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-02-19 18:47:50 +0100

    rollback blueprint resources

--- src/domains/blueprint-app/02_namespace_domain.tf
+++ src/domains/blueprint-app/02_namespace_domain.tf
@@ -20,15 +20,15 @@ module "domain_pod_identity" {
   certificate_permissions = ["Get"]
 }
 
-# resource "helm_release" "reloader" {
-#   name       = "reloader"
-#   repository = "https://stakater.github.io/stakater-charts"
-#   chart      = "reloader"
-#   version    = "v1.0.30"
-#   namespace  = kubernetes_namespace.domain_namespace.metadata[0].name
+resource "helm_release" "reloader" {
+   name       = "reloader"
+   repository = "https://stakater.github.io/stakater-charts"
+   chart      = "reloader"
+   version    = "v1.0.30"
+   namespace  = kubernetes_namespace.domain_namespace.metadata[0].name
 
-#   set {
-#     name  = "reloader.watchGlobally"
-#     value = "false"
-#   }
-# }
+   set {
+     name  = "reloader.watchGlobally"
+     value = "false"
+   }
+ }
