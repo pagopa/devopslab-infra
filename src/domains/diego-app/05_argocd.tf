5ec4291beba11491583af78a7f1ff20c251fb7eb
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-02-20 15:26:20 +0100

    added argocd terraform file

---
+++ src/domains/diego-app/05_argocd.tf
@@ -0,0 +1,18 @@
+#
+# Terraform argocd project
+#
+resource "kubernetes_manifest" "argocd_project_terraform" {
+  manifest = yamldecode(templatefile("${path.module}/argocd/projects/project-terraform-argocd.yaml", {}))
+}
+
+#
+# APPS
+#
+resource "kubernetes_manifest" "argocd_app_status_standalone" {
+  manifest = yamldecode(templatefile("${path.module}/argocd/apps/app-status-standalone.yaml", {}))
+}
+
+resource "kubernetes_manifest" "argocd_apps_ok" {
+  manifest = yamldecode(templatefile("${path.module}/argocd/apps/apps-terraform-ok.yaml", {}))
+}
+
