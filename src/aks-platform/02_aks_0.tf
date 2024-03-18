ea207cd0b7875700e91e6755ec2fcb830c545bf7
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-18 20:51:20 +0100

    aks updated module

--- src/aks-platform/02_aks_0.tf
+++ src/aks-platform/02_aks_0.tf
@@ -14,7 +14,7 @@ resource "azurerm_resource_group" "rg_aks_backup" {
 
 
 module "aks" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_cluster?ref=aks_storage_drivers"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_cluster?ref=v7.70.0"
 
   count = var.aks_enabled ? 1 : 0
 
