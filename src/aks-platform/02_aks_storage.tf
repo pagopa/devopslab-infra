ab991089294b0e442ec9005e08b952c6f0128878
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-03-19 11:27:13 +0100

    aded pvc for fileshare

--- src/aks-platform/02_aks_storage.tf
+++ src/aks-platform/02_aks_storage.tf
@@ -1,3 +1,35 @@
 module "aks_storage_class" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_storage_class?ref=v7.69.1"
 }
+
+# resource "kubernetes_persistent_volume_claim_v1" "blueprint_file_share_premium_zrs" {
+#   metadata {
+#     name = "blueprint-file-share-premium-zrs"
+#     namespace = "blueprint"
+#   }
+#   spec {
+#     access_modes = ["ReadWriteMany"]
+#     resources {
+#       requests = {
+#         storage = "100Gi"
+#       }
+#     }
+#     storage_class_name = "azurefile-premium-zrs"
+#   }
+# }
+
+resource "kubernetes_persistent_volume_claim_v1" "blueprint_file_share_zrs" {
+  metadata {
+    name = "blueprint-file-share-zrs"
+    namespace = "blueprint"
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+    storage_class_name = "azurefile-zrs"
+  }
+}
