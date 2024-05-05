879a132f91d79535fcbd58b8b20cf83c6574aaf3
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-05-05 20:10:37 +0200

    added azure devops agent

--- src/core/04_azure_devops_agent.tf
+++ src/core/04_azure_devops_agent.tf
@@ -15,18 +15,18 @@ module "azdoa_snet" {
   virtual_network_name                      = module.vnet_italy.name
   private_endpoint_network_policies_enabled = true
 }
-#
-#
-# module "azdoa_vmss_li" {
-#   source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=v8.5.0"
-#   count               = var.enable_azdoa ? 1 : 0
-#   name                = local.azuredevops_agent_vm_name
-#   resource_group_name = azurerm_resource_group.azdo_rg[0].name
-#   subnet_id           = module.azdoa_snet[0].id
-#   subscription_name   = data.azurerm_subscription.current.display_name
-#   subscription_id     = data.azurerm_subscription.current.subscription_id
-#   location            = var.location
-#   source_image_name   = var.azdoa_image_name
-#
-#   tags = var.tags
-# }
+
+
+module "azdoa_vmss_li" {
+  source              = "git::https://github.com/pagopa/terraform-azurerm-v3.git//azure_devops_agent?ref=v8.5.0"
+  count               = var.enable_azdoa ? 1 : 0
+  name                = local.azuredevops_agent_vm_name
+  resource_group_name = azurerm_resource_group.azdo_rg[0].name
+  subnet_id           = module.azdoa_snet[0].id
+  subscription_name   = data.azurerm_subscription.current.display_name
+  subscription_id     = data.azurerm_subscription.current.subscription_id
+  location            = var.location_ita
+  source_image_name   = var.azdoa_image_name
+
+  tags = var.tags
+}
