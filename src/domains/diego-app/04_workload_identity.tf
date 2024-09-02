0480ecaa7ee4d1237be309ec82dac711fae77ee6
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-02 18:12:54 +0200

    updated modules

--- src/domains/diego-app/04_workload_identity.tf
+++ src/domains/diego-app/04_workload_identity.tf
@@ -1,7 +1,7 @@
 module "workload_identity" {
-  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_workload_identity?ref=v8.39.0"
+  source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_workload_identity_configuration?ref=v8.42.0"
 
-  workload_name_prefix                  = var.domain
+  workload_identity_name                = "${var.domain}-workload-identity"
   workload_identity_resource_group_name = data.azurerm_kubernetes_cluster.aks.resource_group_name
   aks_name                              = data.azurerm_kubernetes_cluster.aks.name
   aks_resource_group_name               = data.azurerm_kubernetes_cluster.aks.resource_group_name
@@ -12,3 +12,4 @@ module "workload_identity" {
   key_vault_key_permissions         = ["Get"]
   key_vault_secret_permissions      = ["Get"]
 }
+
