18bbd07ff1ff4e04a7a94dff790117f1811f5a96
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-09-02 18:47:38 +0200

    pre-commit fixs

--- src/aks-platform/05_keda.tf
+++ src/aks-platform/05_keda.tf
@@ -23,7 +23,7 @@ module "keda_workload_identity_init" {
 module "keda_workload_identity_configuration" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_workload_identity_configuration?ref=v8.42.0"
 
-  workload_identity_name                 = module.keda_workload_identity_init.user_assigned_identity_name
+  workload_identity_name                = module.keda_workload_identity_init.user_assigned_identity_name
   workload_identity_resource_group_name = azurerm_resource_group.rg_aks.name
   aks_name                              = module.aks.name
   aks_resource_group_name               = azurerm_resource_group.rg_aks.name
