b0183c49016d73dd2fe0ce8bd4716f18169bd533
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-02-03 10:30:57 +0100

    prometheus changed module version

--- src/aks-platform/03_monitoring.tf
+++ src/aks-platform/03_monitoring.tf
@@ -6,7 +6,7 @@ resource "kubernetes_namespace" "monitoring" {
 }
 
 module "aks_prometheus_install" {
-  source               = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_prometheus_install?ref=PAYMCLOUD-223-prometheus-miglioramento-configurazione"
+  source               = "git::https://github.com/pagopa/terraform-azurerm-v3.git//kubernetes_prometheus_install?ref=v8.78.0"
   prometheus_namespace = kubernetes_namespace.monitoring.metadata[0].name
   storage_class_name   = "default-zrs"
   prometheus_helm = {
