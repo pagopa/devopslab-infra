37a8b3ccd10aae4d34598fa93145b4e317518769
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-08-05 16:01:23 +0200

    pre-commit fixs

--- src/aks-platform/10_argocd.tf
+++ src/aks-platform/10_argocd.tf
@@ -27,13 +27,13 @@ resource "helm_release" "argocd" {
 
   values = [
     templatefile("${path.module}/argocd/argocd_helm_setup_values.yaml", {
-      ARGOCD_APPLICATION_NAMESPACES = var.argocd_application_namespaces
-      TENANT_ID                     = data.azurerm_subscription.current.tenant_id
-      APP_CLIENT_ID                 = data.azurerm_key_vault_secret.argocd_entra_app_client_id.value
-      ENTRA_ADMIN_GROUP_OBJECT_ID   = data.azuread_group.adgroup_admin.object_id
-      ARGOCD_INTERNAL_URL          = local.argocd_internal_url
-      ARGOCD_INGRESS_TLS_SECRET_NAME   = replace(local.argocd_internal_url, ".", "-", )
-      FORCE_REINSTALL               = var.argocd_force_reinstall_version
+      ARGOCD_APPLICATION_NAMESPACES  = var.argocd_application_namespaces
+      TENANT_ID                      = data.azurerm_subscription.current.tenant_id
+      APP_CLIENT_ID                  = data.azurerm_key_vault_secret.argocd_entra_app_client_id.value
+      ENTRA_ADMIN_GROUP_OBJECT_ID    = data.azuread_group.adgroup_admin.object_id
+      ARGOCD_INTERNAL_URL            = local.argocd_internal_url
+      ARGOCD_INGRESS_TLS_SECRET_NAME = replace(local.argocd_internal_url, ".", "-", )
+      FORCE_REINSTALL                = var.argocd_force_reinstall_version
     })
   ]
 
